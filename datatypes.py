a9b03783dfc842e1330240bbd52eed309dececa4
goonmandu <hwang.mingi7@gmail.com>
2024-02-04 14:33:08 +0900

    Edit NameAndAvg to accept unsolved count

--- datatypes.py
+++ datatypes.py
@@ -1,11 +1,26 @@
 class NameAndAvg:
-    def __init__(self, n, a, s):
+    def __init__(self, n, a, s, u):
         self.name = n
         self.average = a
         self.samples = s
+        self.unsolved = u
+        self.solved = s - u
 
     def __str__(self):
-        return f"`{format(round(self.average, 2), '.2f')}` - {self.name} ({self.samples})"
+        return f"`{format(round(self.average, 2), '.2f')}` - {self.name} "\
+               f"({self.solved}/{self.samples} : {round(self.solved * 100 / self.samples, 2)}%)"
+
+    def avgstr(self):
+        return f"{format(round(self.average, 2), '.2f')}"
+
+    def namestr(self):
+        return self.name
+
+    def fracstr(self):
+        return f"{self.solved}/{self.samples}"
+
+    def pctstr(self):
+        return f"{format(round(self.solved * 100 / self.samples, 2), '.2f')}%"
 
 
 class PathAndCreationTime:
